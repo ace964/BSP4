db0b16f822073e23039e152b28b7da460672ad22
Max Mang <maximilian.mang@haw-hamburg.de>
2017-12-17 16:04:57 +0100

    fixed errors

--- TZM.c
+++ TZM.c
@@ -4,15 +4,17 @@
  * @date   16.12.2017
  */
 
-#include <linux/init.h>           // Macros used to mark up functions e.g. __init __exit
-#include <linux/module.h>         // Core header for loading LKMs into the kernel
-#include <linux/device.h>         // Header to support the kernel Driver Model
-#include <linux/kernel.h>         // Contains types, macros, functions for the kernel
-#include <linux/fs.h>             // Header for the Linux file system support
-#include <asm/uaccess.h>          // Required for the copy to user function
-#include <linux/mutex.h>          // Mutex
+#include <linux/types.h>
+#include <linux/string.h>
+#include <linux/slab.h>
 #include <linux/jiffies.h>
-#include <linux/math64.h>
+#include <linux/fs.h>
+#include <linux/module.h>
+#include <linux/cdev.h>
+#include <asm/uaccess.h>
+#include <linux/mutex.h>
+#include <linux/moduleparam.h>
+#include <linux/stat.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Frederic Dlugi and Maximilian Mang");
@@ -26,12 +28,13 @@ static short  size_of_message;              ///< Used to remember the size of th
 unsigned long ret_val_time = 0;
 unsigned long time_since_last_newline = 0;
 int ret_val_number = 0;
-int dev_open = 0;
 module_param(ret_val_time, long, S_IRUGO);
 MODULE_PARM_DESC(ret_val_time, "Zeit zwischen zwei newlines, wenn keine Messung bekannt ist");
 module_param(ret_val_number, int, S_IRUGO);
 MODULE_PARM_DESC(ret_val_number, "Laenge zwischen zwei newline, wenn keine Messung bekannt ist");
 
+//flag to ensure only one opening
+static bool isOpen = false;
 
 // The prototype functions for the character driver -- must come before the struct definition
 static int     dev_open(struct inode *, struct file *);
@@ -61,7 +64,7 @@ static int __init dev_init(void)
 	alloc_chrdev_region(&dev_number, 0, 1, "tzm");
 	driver_object = cdev_alloc();
     driver_object->ops = &fops;
-    driver_object->owner = "tzm";
+    driver_object->owner = THIS_MODULE;
 	cdev_add(driver_object, dev_number, 1);
     return 0;
 }
@@ -87,12 +90,11 @@ static int dev_open(struct inode *inodep, struct file *filep){
         printk(KERN_INFO "Device Could not be opened. Already Opened\n");
         return -EBUSY;
     }
-	if(dev_open)
-	{
-		return -EBUSY;
-	}
-	
-	dev_open = 1;
+    if(isOpen){
+        return -EBUSY;
+    }
+    
+    isOpen = true;
 	ret_val_number = 0;
 	ret_val_time = 0;
     printk(KERN_INFO "Opened\n");
@@ -109,22 +111,23 @@ static int dev_open(struct inode *inodep, struct file *filep){
  *  @param offset The offset if required
  */
 static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset){
-	if(mutex_lock_interruptible(&lock_mutex))
+	int error_count = 0;
+    if(mutex_lock_interruptible(&lock_mutex))
         return 0; // nothing read
 
 	
-	int error_count = 0;
+	
 	// copy_to_user has the format ( * to, *from, size) and returns 0 on success
 	error_count = copy_to_user(buffer, message, size_of_message);
 
-	if (error_count==0){            // if true then have success
+	if (error_count == 0){            // if true then have success
 		printk(KERN_INFO "charDriver: Sent %d characters to the user\n", size_of_message);
-		mutex_unlock(&access_mutex);
+		mutex_unlock(&lock_mutex);
 		return (size_of_message=0);  // clear the position to the start and return 0
 	}
 	else {
 		printk(KERN_INFO "charDriver: Failed to send %d characters to the user\n", error_count);
-		mutex_unlock(&access_mutex);
+		mutex_unlock(&lock_mutex);
 		return -EFAULT;              // Failed -- return a bad address message (i.e. -14)
 	}
 	
@@ -139,12 +142,12 @@ static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *of
  *  @param offset The offset if required
  */
 static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *offset){
-	
+	int i; // Initialized at top because of 
 	if(mutex_lock_interruptible(&lock_mutex))
         return 0; // nothing copied
 	
 	
-	int i;
+	
 	for(i = 0; i < len; i++)
 	{
 		ret_val_number++;
@@ -158,7 +161,7 @@ static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, lof
 		}
 	}
 	
-	sprintf(message,"Time =%d Chars=%d",time_since_last_newline, ret_val_number);
+	sprintf(message,"Time =%lu Chars=%d",time_since_last_newline, ret_val_number);
 	// appending received string with its length
     size_of_message = strlen(message);                 // store the length of the stored message
     printk(KERN_INFO "charDriver: Received %zu characters from the user\n", len);
@@ -176,7 +179,7 @@ static int dev_release(struct inode *inodep, struct file *filep){
 	{
         return -EBUSY;
     }
-	dev_open = 0;
+	isOpen = false;
 	mutex_unlock(&lock_mutex);
 	printk(KERN_INFO "Device successfully closed\n");
 	return 0;
